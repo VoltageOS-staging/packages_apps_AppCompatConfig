28c2f0a3428117bd2e349a73feafec2b9317b3f7
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-30 18:45:15 -0400

    update Protobuf to 4.31.1

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 }
 
 dependencies {
-    implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
+    implementation("com.google.protobuf:protobuf-kotlin:4.31.1")
     protobuf(files("proto"))
     implementation(kotlin("stdlib-jdk8"))
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.25.1"
+        artifact = "com.google.protobuf:protoc:4.31.1"
     }
 }
 
