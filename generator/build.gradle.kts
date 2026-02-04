ac7139038bdb09192d9bbdc3eba859abd53e4244
Daniel Micay <daniel.micay@grapheneos.org>
2026-02-03 19:26:25 -0500

    update Kotlin to 2.3.0

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.google.protobuf.gradle.*
 
 plugins {
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.3.0"
     id("application")
     id("com.google.protobuf") version "0.9.5"
 }
