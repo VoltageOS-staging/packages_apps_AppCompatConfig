7c357d3961b485be2e38d5c818817c4d0de88ad0
Thomas <thomas.giudici@proton.me>
2026-05-09 14:25:24 +0200

    Remove unused import from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.internal.utils.getBuildSrcPlugins
-
 plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.google.protobuf") version "0.10.0" apply false
