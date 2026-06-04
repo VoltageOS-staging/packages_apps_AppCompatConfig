592b38ed90a92b6aa2eb0d425c97f2c5a7e45965
Daniel Micay <daniel.micay@grapheneos.org>
2026-06-04 19:31:11 -0400

    update Android Gradle plugin to 9.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.android.build.gradle.internal.utils.getBuildSrcPlugins
 
 plugins {
-    id("com.android.application") version "9.0.0" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("com.google.protobuf") version "0.10.0" apply false
 }
 
